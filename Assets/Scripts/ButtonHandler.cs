35ffe5adb25773d341ea331c98df9fc288063e6d
bjohnson66 <bjohnson66@uiowa.edu>
2022-10-15 23:16:46 -0500

    button works

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -1,9 +1,13 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
 {
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,22 @@ public class ButtonHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+     
+    }
+    */
+    //Take to Battle 
+    public Button startButton;
+
+    private void Start()
+    {
+        Button btn = startButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Battle");
+        Debug.Log("button clicked!");
     }
 }
